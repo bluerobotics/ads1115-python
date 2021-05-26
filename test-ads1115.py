a75d0e39717afac4f7245b8fb0cd4c5d181eeb49
Jacob Walser <jwalser90@gmail.com>
2021-05-26 16:32:02 -0400

    test-ads1115: add proper signal handling

--- test-ads1115.py
+++ test-ads1115.py
@@ -2,6 +2,7 @@
 
 from ads1115 import ADS1115
 import argparse
+import signal
 import time
 
 parser = argparse.ArgumentParser(description='ads1115 test')
@@ -11,20 +12,27 @@ args = parser.parse_args()
 
 ads = ADS1115()
 
+outfile = None
+
 if args.output:
     outfile = open(args.output, "w")
+
+
+def cleanup(_signo, _stack):
+    if outfile:
+        outfile.close()
+    exit(0)
+
+
+signal.signal(signal.SIGTERM, cleanup)
+signal.signal(signal.SIGINT, cleanup)
 
 while True:
     for channel in range(4):
         output = f"{time.time()} 1 {channel} {ads.read(channel)}"
         print(output)
-        if args.output:
+        if outfile:
             outfile.write(output)
             outfile.write('\n')
         if args.frequency:
             time.sleep(1.0/args.frequency)
-
-# this is never reached, but works anyway in practice
-# todo handle KeyboardInterrupt for ctrl+c
-if args.output:
-    outfile.close()
