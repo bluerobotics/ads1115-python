d3324a6edfb1b0629780eff17a5799481b0ff785
Jacob Walser <jwalser90@gmail.com>
2021-09-01 01:21:48 -0400

    report: update/refactor to export generate_figures()

--- ads1115/report.py
+++ ads1115/report.py
@@ -1,54 +1,47 @@
 #!/usr/bin/python3
 
-import argparse
-from llog import LLogReader
-from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+def generate_figures(log):
+    footer = 'ads1115 test report'
 
-defaultMeta = dir_path + '/ads1115.meta'
-parser = argparse.ArgumentParser(description='ads1115 test report')
-parser.add_argument('--input', action='store', type=str, required=True)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--output', action='store', type=str)
-parser.add_argument('--show', action='store_true')
-args = parser.parse_args()
+    f, spec = log.figure(height_ratios=[1,1], suptitle='ads1115 data', footer=footer)
+    plt.subplot(spec[0,0])
+    log.channel0.pplot()
 
-log = LLogReader(args.input, args.meta)
+    plt.subplot(spec[0,1])
+    log.channel1.pplot()
 
-pdf = None
-if args.output:
-    outfile = open('ads1115.pdf', 'wb')
-    pdf = PdfPages(outfile)
+    plt.subplot(spec[1,0])
+    log.channel2.pplot()
 
-def saveFig():
-    if pdf:
-        pdf.savefig()
+    plt.subplot(spec[1,1])
+    log.channel3.pplot()
 
-def closeFig():
-    if pdf:
-        pdf.close()
+if __name__ == 'main':
+    import argparse
+    from llog import LLogReader
+    from matplotlib.backends.backend_pdf import PdfPages
+    import os
 
-footer = 'ads1115 test report'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-f, spec = log.figure(height_ratios=[1,1], suptitle='ads1115 data statistics', footer=footer)
-plt.subplot(spec[0,0])
-log.channel0.pplot()
+    defaultMeta = dir_path + '/ads1115.meta'
+    parser = argparse.ArgumentParser(description='ads1115 test report')
+    parser.add_argument('--input', action='store', type=str, required=True)
+    parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
+    parser.add_argument('--output', action='store', type=str)
+    parser.add_argument('--show', action='store_true')
+    args = parser.parse_args()
 
-plt.subplot(spec[0,1])
-log.channel1.pplot()
+    log = LLogReader(args.input, args.meta)
 
-plt.subplot(spec[1,0])
-log.channel2.pplot()
+    generate_figures(log)
 
-plt.subplot(spec[1,1])
-log.channel3.pplot()
+    if args.output:
+        # todo check if it exists!
+        with PdfPages(args.output, metadata=log.meta) as pdf:
+            [pdf.savefig(n) for n in plt.get_fignums()]
 
-saveFig()
-
-closeFig()
-
-if args.show:
-    plt.show()
+    if args.show:
+        plt.show()
