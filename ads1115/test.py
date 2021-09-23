d27e1d54caa8de1d5a98a2e0d30ce803d0977f2e
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:40:41 +1000

    test-ads1115: use default parser

--- ads1115/test.py
+++ ads1115/test.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
-def main():
-    from ads1115 import ADS1115
-    import argparse
-    from llog import LLogWriter
-    import time
-    from pathlib import Path
+from ads1115 import ADS1115
+from llog import LLogWriter
+import time
 
-    device = "ads1115"
-    defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
+device = "ads1115"
+parser = LLogWriter.create_default_parser(__file__, device)
+args = parser.parse_args()
 
-    parser = argparse.ArgumentParser(description=f'{device} test')
-    parser.add_argument('--output', action='store', type=str, default=None)
-    parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-    parser.add_argument('--frequency', action='store', type=int, default=None)
-    args = parser.parse_args()
+with LLogWriter(args.meta, args.output) as log:
+    ads = ADS1115()
 
-    with LLogWriter(args.meta, args.output) as log:
-        ads = ADS1115()
+    # requires custom channel setup - doesn't use standard data logging
+    LLOG_CH0 = 100
 
-        LLOG_CH0 = 100
+    if args.frequency:
+        delay = 1 / args.frequency
 
-        while True:
-            for channel in range(4):
-                log.log(LLOG_CH0 + channel, ads.read(channel))
-                if args.frequency:
-                    time.sleep(1.0/args.frequency)
+    start_time = time.time()
+    while time.time() - start_time < args.duration:
+        for channel in range(4):
+            try:
+                self.log(LLOG_CH0 + channel, ads.read(channel))
+            except Exception as e:
+                self.log_error(e, measurement_time)
+                if args.stop_on_error:
+                    return
 
-if __name__ == '__main__':
-    main()
+            if args.frequency:
+                time.sleep(delay)
