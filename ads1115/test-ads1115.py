4d7c273c1252e0eb465b853b7da327ab38cb2cf4
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:40:41 +1000

    test-ads1115: use default parser

--- ads1115/test-ads1115.py
+++ ads1115/test-ads1115.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 
 from ads1115 import ADS1115
-import argparse
 from llog import LLogWriter
 import time
-from pathlib import Path
 
 device = "ads1115"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=None)
+parser = LLogWriter.create_default_parser(__file__, device)
 args = parser.parse_args()
 
 with LLogWriter(args.meta, args.output) as log:
     ads = ADS1115()
 
+    # requires custom channel setup - doesn't use standard data logging
     LLOG_CH0 = 100
 
-    while True:
+    if args.frequency:
+        delay = 1 / frequency
+        
+    start_time = time.time()
+    while time.time() - start_time < args.duration:
         for channel in range(4):
-            log.log(LLOG_CH0 + channel, ads.read(channel))
+            try:
+                self.log(LLOG_CH0 + channel, ads.read(channel))
+            except Exception as e:
+                self.log_error(e, measurement_time)
+                if args.stop_on_error:
+                    return
+
             if args.frequency:
-                time.sleep(1.0/args.frequency)
+                time.sleep(delay)
