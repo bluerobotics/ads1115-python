e0b36ef1bee9b15bcb58bd674cebf42b3a00eb7b
Jacob Walser <jwalser90@gmail.com>
2021-09-28 03:50:41 -0400

    test: refactor for setup entry_points

--- ads1115/test.py
+++ ads1115/test.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
 
-from ads1115 import ADS1115
-from llog import LLogWriter
-import time
+def main():
+    from ads1115 import ADS1115
+    from llog import LLogWriter
+    import time
 
-device = "ads1115"
-parser = LLogWriter.create_default_parser(__file__, device)
-args = parser.parse_args()
+    device = "ads1115"
+    parser = LLogWriter.create_default_parser(__file__, device)
+    args = parser.parse_args()
 
-with LLogWriter(args.meta, args.output) as log:
-    ads = ADS1115()
+    with LLogWriter(args.meta, args.output) as log:
+        ads = ADS1115()
 
-    # requires custom channel setup - doesn't use standard data logging
-    LLOG_CH0 = 100
+        # requires custom channel setup - doesn't use standard data logging
+        LLOG_CH0 = 100
 
-    if args.frequency:
-        delay = 1 / args.frequency
+        if args.frequency:
+            delay = 1 / args.frequency
 
-    start_time = time.time()
-    while time.time() - start_time < args.duration:
-        for channel in range(4):
-            try:
-                self.log(LLOG_CH0 + channel, ads.read(channel))
-            except Exception as e:
-                self.log_error(e, measurement_time)
-                if args.stop_on_error:
-                    return
+        start_time = time.time()
+        while time.time() - start_time < args.duration:
+            for channel in range(4):
+                try:
+                    self.log(LLOG_CH0 + channel, ads.read(channel))
+                except Exception as e:
+                    self.log_error(e, measurement_time)
+                    if args.stop_on_error:
+                        return
 
-            if args.frequency:
-                time.sleep(delay)
+                if args.frequency:
+                    time.sleep(delay)
+
+if __name__ == '__main__':
+    main()
